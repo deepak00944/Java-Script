542a960f94532c8ebc96ff59b5ee7bfc4bb97e01
Deepak Singh <deepak.s.ashta@gmail.com>
2023-11-07 13:04:40 +0530

    updated

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -13,17 +13,17 @@ const gameName = new String("deepak-ds-jsk")
 const url = "https://deepak.com/deepak-10"
 const spaceVar = "     kyaaa hai          "
 
-console.log(gameName.anchor("hiihello"))  //<a name="hiihello">deepakds</a>
+console.log(gameName.anchor("hiihello"))  //<a name="hiihello">deepak-ds-jsk</a>
 console.log(gameName.at(3)); //p
 console.log(gameName.big()) //<big>deepakds</big>
 console.log(gameName.bold()) //<b>deepakds</b>
 console.log(gameName.slice(0,5)) //deepa
-console.log(gameName) //[String: 'deepakds']
+console.log(gameName) //[String: 'deepak-ds-jsk']
 console.log(gameName[0]); // d
 console.log(gameName.__proto__);
 console.log(gameName.length);//13
 console.log(gameName.toUpperCase());//DEEPAK-DS-JSK
-console.log(gameName.split("-")) //[ 'deepak', 'ds', 'jsk' ]
+console.log(gameName.split("-")) //[ 'deepak', 'ds', 'jsk' ] this will give the array
 console.log(gameName.substring(2,8)); ///epak-d
 console.log(gameName.replace("d","p"))//peepak-ds-jsk
 console.log(gameName.replaceAll("d","p")) //peepak-ps-jsk
